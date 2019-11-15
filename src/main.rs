b8ed864dd5877db3c0fb2e268e91da4d09a3626b
Denis Igorevich Lobanov <denis.lobanov@zooei.com>
2019-11-15 18:53:07 +0000

    basic single-file analysis

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,62 @@
+extern crate clap;
+
 use std::io;
 use std::fs::File;
 use std::collections::HashMap;
+use std::io::Read;
+use clap::{App, Arg};
 
-fn readFile(path: String) -> std::result::Result<String, std::io::Error> {
+fn read_file(path: &str) -> io::Result<String> {
     let mut s = String::new();
     File::open(path)?.read_to_string(&mut s)?;
-    Ok(s);
+    Ok(s)
 }
 
-fn wordFrequency(corpus: &str) -> HashMap<String, u64> {
+fn word_frequency(corpus: &str) -> HashMap<&str, u64> {
     let mut map = HashMap::new();
-    corpus.split
+    let words: Vec<&str> = corpus.split_whitespace()
+        .map(|w| w.trim().trim_matches(|c| c == '.' || c == ','))
+        .collect();
 
+    for word in words.into_iter() {
+        if map.contains_key(&word) {
+            *map.get_mut(&word).unwrap() += 1;
+        } else {
+            map.insert(word, 1);
+        }
+    }
 
-
-    //corpus.split_whitespace().map(|x: Some(String)| -> x.box
+    return map
 }
 
 fn main() {
-    println!("aaeusntaohu");
-    assert!(!'\n'.is_whitespace());
+    let matches = App::new("rustygenmo")
+        .version("0.0.1")
+        .author("Denis Lobanov")
+        .about("nanogenmo 2019 entry")
+        .arg(Arg::with_name("file")
+            .short("f")
+            .value_name("FILE")
+            .help("The file to parse")
+            .takes_value(true))
+        .get_matches();
+
+    if let Some(f) = matches.value_of("file") {
+        let corpus = read_file(f);
+        if let Ok(c) = corpus {
+            let freq = word_frequency(c.as_str());
+
+            let mut result: Vec<(&str, u64)> = Vec::new();
+            freq.into_iter()
+                .for_each(|e| result.push(e));
+
+            // sort
+            result.sort_by(|(_,v1), (_, v2)| v1.cmp(v2));
+            result.reverse();
+            result.iter()
+                .for_each(|(k,v)| println!("[{}] {}", k, v));
+        }
+    } else {
+        eprintln!("need to give a file")
+    }
 }
