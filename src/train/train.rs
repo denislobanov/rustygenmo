6c7e8d29e3b04166b3c9b5ad4c4c93afa2141402
Denis Igorevich Lobanov <denis.lobanov@zooei.com>
2019-11-20 00:45:22 +0000

    Initial grouping with full lookup
    Shitt O(n^2) implementation, lots of cleanup and proper error handling
    todo, but basically works.
    
    Can calculate word frequency, frequency groups and build a revere map of
    word -> most common frequency group. Based on this group it should now
    be possible to start building m * n-gram chains, where m is the number
    of groups used and n is in the set of top most common groups seen.

--- src/train/train.rs
+++ src/train/train.rs
@@ -7,15 +7,16 @@ use crate::train::analyse::create_corpus;
 use std::convert::TryInto;
 use std::str::from_utf8;
 use std::borrow::Borrow;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use self::sled::IVec;
 
 pub struct Persistent {
     db: Db
 }
 
 impl Persistent {
-    pub fn groups(&mut self, _count: usize, files: Vec<PathBuf>) -> () {
-        let t = self.db.open_tree("groups").unwrap(); //todo
+    pub fn groups(&mut self, count: usize, files: Vec<PathBuf>) -> () {
+        let t = self.db.open_tree("words").unwrap(); //todo
 
         for file in files {
             let data = read_file(file).unwrap(); //todo error handling
@@ -30,7 +31,7 @@ impl Persistent {
         // calculate groups
         let mut map: HashMap<u32, u32> = HashMap::new();
 
-        t.iter().into_iter().for_each(|x| {
+        t.iter().for_each(|x| {
             if x.is_err() {
                 panic!("wtf {}", x.err().unwrap())
             }
@@ -45,8 +46,48 @@ impl Persistent {
             }
         });
 
+        // calculate top /count/ groups
+        let mut gs: Vec<(u32, u32)> = Vec::new();
+        map.into_iter().for_each(|e| gs.push(e));
+        gs.sort_by(|(_, v1), (_, v2)| v2.cmp(v1));
+
+        let mut tops: HashSet<u32> = HashSet::new();
+        let mut last: u32 = 0;
+        for (g, _) in gs.into_iter().take(count) {
+            tops.insert(g);
+            last = g;
+        }
+
+        // persist word -> group map for top _count_ groups
+        //TODO: refactor to store words with group counts to avoid O(n2)
+        let groups = self.db.open_tree("groups").unwrap();
+
+        t.iter().for_each(|x| {
+            // already checked for errors on previous loop :'(
+
+            match x.ok() {
+                Some((k, v)) => {
+                    let key = from_utf8(k.borrow()).unwrap();
+                    let a: [u8; 4] = v.iter().as_slice().try_into().unwrap();
+                    let count = u32::from_be_bytes(a);
+
+                    if tops.contains(&count) {
+                        groups.insert(key, v).unwrap(); //todo err handling
+                    } else {
+                        groups.insert(key, u32_to_ivec(last)).unwrap();
+                    }
+
+                    return;
+                },
+                None => panic!("again with the empty!"),
+            }
+        });
+
+        // we dont need the word frequency tree anymore
+        self.db.drop_tree(b"words").unwrap();
+
         // print some data! (for now)
-        map.iter().for_each(|(k, v)| println!("{} {}", *k, *v))
+        print_tree(groups)
     }
 }
 
@@ -68,4 +109,24 @@ fn inc(v: Option<&[u8]>) -> Option<Vec<u8>> {
     };
 
     Some(count.to_be_bytes().to_vec())
+}
+
+fn print_tree(t: sled::Tree) -> () {
+    t.iter().for_each(|r| {
+        if let Ok(e) = r {
+            match e {
+                (k, v) => {
+                    let key = from_utf8(k.borrow()).unwrap();
+                    let a: [u8; 4] = v.iter().as_slice().try_into().unwrap();
+                    let value = u32::from_be_bytes(a);
+
+                    println!("{} {}", key, value)
+                }
+            }
+        }
+    })
+}
+
+fn u32_to_ivec(x: u32) -> IVec {
+    IVec::from(x.to_be_bytes().to_vec())
 }
